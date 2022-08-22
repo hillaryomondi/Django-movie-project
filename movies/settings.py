9f7c4232cf843ae8e5f2dce8f03a68af751a3019
hillaryomondi <hillary.okoth@strathmore.edu>
2022-08-22 19:42:42 +0300

    Added create and delete functions and templates

--- movies/settings.py
+++ movies/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-*5ar8aojou)bd&$dv#sfniim!&60in0d-miap&!5hf(yw1+08z'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
